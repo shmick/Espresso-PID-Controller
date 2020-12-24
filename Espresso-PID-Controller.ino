5061a530ea27de7713b48f6eef6cb7d5a1b18298
shmick <github@warning.ca>
2020-12-23 19:50:24 -0500

    cleanup

--- Espresso-PID-Controller.ino
+++ Espresso-PID-Controller.ino
@@ -5,31 +5,22 @@
   PID Library: https://github.com/br3ttb/Arduino-PID-Library
   PID Lab: https://www.pdx.edu/nanogroup/sites/www.pdx.edu.nanogroup/files/2013_Arduino%20PID%20Lab_0.pdf
   Smoothing: https://www.arduino.cc/en/Tutorial/Smoothing
-  Arduino <> Wemos D1 Mini pins: https://github.com/esp8266/Arduino/blob/master/variants/d1_mini/pins_arduino.h
-  ESP8266 file uploading https://tttapa.github.io/ESP8266/Chap12%20-%20Uploading%20to%20Server.html
 
   Hardware:
-  Wemos D1 Mini ( https://wiki.wemos.cc/products:d1:d1_mini )
+  Wemos D1 Mini ( https://www.wemos.cc/en/latest/d1/d1_mini.html )
   128 x 64 OLED Display using Adafruit_SSD1306 library
   AD8495 Thermocouple Amplifier ( https://www.adafruit.com/product/1778 )
-  ADS1115 16-Bit ADC using library from https://github.com/baruch/ADS1115
+  ADS1115 16-Bit ADC 
   Solid State Relay ( Crydom TD1225 )
 */
 
-#define CONFIG_VERSION "2020-12-20-r00"
+#include <PID_v1.h>
 
-const int ADSGAIN = 2;
-
-// Measured output of the TLVH431 1.25V precision voltage regulator
-const double Vref = 1.2516;
+// Needed for I2C
+#include <Wire.h>
 
 // set to 1 if using the OLED display. Disabled by default.
 #define OLED_DISPLAY 0
-
-#include <PID_v1.h>
-
-// Needed for the I2C ports
-#include <Wire.h>
 
 #if OLED_DISPLAY == 1
 #include <Adafruit_GFX.h>
@@ -42,7 +33,7 @@ const double Vref = 1.2516;
 #include <DNSServer.h>
 #include <IotWebConf.h>
 
-// Needed for ADS1115 ADC
+// Needed for ADS1115 ADC - https://github.com/baruch/ADS1115
 #include "src/ADS1115/ADS1115.h"
 
 // Needed to debounce the steam switch https://github.com/JChristensen/JC_Button
@@ -52,11 +43,18 @@ const double Vref = 1.2516;
 // * Config options that you can customize *
 // *****************************************
 
+// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
+const char thingName[] = "espresso";
+// -- Initial password to connect to the Thing, when it creates an own Access Point.
+const char wifiInitialApPassword[] = "espresso";
+
 #define RelayPin D2
 #define SteamPin D4
 
+// Default to being ON
+bool operMode = false; // keep things off by default while we're testing
+
 // After powering on, how many minutes until we force the boiler to power down
-// Turning the machine off and on again will reset the timer
 const int maxRunTime = 180;
 
 // Max time in seconds that the steam switch can be toggled from on to off to reset the operMode state to true
@@ -68,9 +66,6 @@ const int steamMaxMins = 10;
 // Turn the display off after 200 minutes
 const int maxDisplayMins = 200;
 
-// Default to being ON
-bool operMode = true;
-
 // Define the PID setpoints
 double Setpoint = 105; // This will be the default coffee setpoint
 
@@ -78,7 +73,6 @@ const double CoffeeSetpoint = Setpoint;
 const double SteamSetpoint = 125;
 
 // Define the PID tuning Parameters
-//double Kp = 3.5; working ok on 2018-09-14
 double Kp = 4.5;
 double Ki = 0.125;
 double Kd = 0.2;
@@ -86,16 +80,16 @@ double Kd = 0.2;
 // PWM Window in milliseconds
 const int WindowSize = 5000;
 
-// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
-const char thingName[] = "espresso";
-// -- Initial password to connect to the Thing, when it creates an own Access Point.
-const char wifiInitialApPassword[] = "espresso";
-
 // ***********************************************************
 // ***********************************************************
 // * There should be no need to tweak many things below here *
 // ***********************************************************
 // ***********************************************************
+
+const int ADSGAIN = 2;
+
+// The AD8495 board uses a 1.25v voltage regulator for Vref
+const double Vref = 1.25;
 
 // PID variables
 // Using P_ON_M mode ( http://brettbeauregard.com/blog/2017/06/introducing-proportional-on-measurement/ )
@@ -113,8 +107,7 @@ int average = 0;           // the average
 
 // Thermocouple variables
 float Vout; // The voltage coming from the out pin on the TC amp
-float Vtc;
-// const float Vbits = brdVolts / 1023; // ** Not needed with ADS1115 board
+float Vtc;  // Placeholder for getting AD8495 value ready to perform the NIST calc
 
 // Corrected temperature readings for a K-type thermocouple
 // https://srdata.nist.gov/its90/type_k/kcoefficients_inverse.html
@@ -140,7 +133,7 @@ IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword);
 String header;
 
 // All timers reference the value of now
-unsigned long now = 0; //This variable is used to keep track of time
+unsigned long now = millis(); //This variable is used to keep track of time
 
 // OLED display timer
 const int OLEDinterval = 250;           // interval at which to write new data to the OLED
@@ -155,7 +148,7 @@ long runTimeSecs;
 unsigned long runTimeStart = now;
 
 // Temp read interval
-const int TempInterval = 5;
+const int TempInterval = 50;
 unsigned long currentTempMillis;
 unsigned long previousTempMillis = now;
 
@@ -189,9 +182,9 @@ Adafruit_SSD1306 display(OLED_RESET);
 #endif
 
 // ADS1115 ADC
-int adcval;
-ADS1115 adc;
+float adcval;
 float ADS_PGA;
+ADS1115 adc;
 
 unsigned long prevLoopMillis;
 int numLoops = 0;
@@ -272,49 +265,68 @@ void readTemps(void)
   }
 }
 
-void relayControl(void)
+// unless true is specified, default to false
+bool enablePID(bool enable = false)
 {
-  // Calculate the number of running minutes
-
-  // If more than maxRunTime minutes has elapsed, turn the boiler off
-  // and dont perform any other PID functions
-  if ((runTimeMins >= maxRunTime) || (!operMode))
+  if (enable == false)
   {
+    // de-activate the relay
     digitalWrite(RelayPin, LOW);
+    // set PID mode to manual mode
     myPID.SetMode(MANUAL);
+    // Force PID output to 0
     Output = 0;
+    // set operation mode to false
     operMode = false;
   }
-  else
+  else if (enable == true)
   {
-    // Compute the PID values
     myPID.SetMode(AUTOMATIC);
+    runTimeStart = now;
+    operMode = true;
+  }
+}
+
+void relayControl(void)
+{
+  // If we've reached maxRunTime, disable the PID control
+  if (runTimeMins >= maxRunTime && operMode)
+  {
+    enablePID(false);
+  }
+  // Set the PID back to Automatic mode if operMode is true
+  else if (!myPID.GetMode() && operMode)
+  {
+    enablePID(true);
+  }
+
+  if (operMode)
+  {
     myPID.Compute();
-  }
 
-  PWMOutput = Output * (WindowSize / 100.00);
-  // Starts a new PWM cycle every WindowSize milliseconds
-  if (now - windowStartTime > WindowSize)
-  { //time to shift the Relay Window
-    windowStartTime += WindowSize;
-  }
+    // Starts a new PWM cycle every WindowSize milliseconds
+    if (now - windowStartTime > WindowSize)
+    { //time to shift the Relay Window
+      windowStartTime += WindowSize;
+    }
 
-  // Calculate the number of milliseconds that have passed in the current PWM cycle.
-  // If that is less than the Output value, the relay is turned ON
-  // If that is greater than (or equal to) the Output value, the relay is turned OFF.
-  if (PWMOutput > (now - windowStartTime))
-  {
-    digitalWrite(RelayPin, HIGH);
-  }
-  else
-  {
-    digitalWrite(RelayPin, LOW);
+    // Calculate the number of milliseconds that have passed in the current PWM cycle.
+    // If that is less than the Output value, the relay is turned ON
+    // If that is greater than (or equal to) the Output value, the relay is turned OFF.
+    PWMOutput = Output * (WindowSize / 100.00);
+    if ((PWMOutput > 100) && (PWMOutput > (now - windowStartTime)))
+    {
+      digitalWrite(RelayPin, HIGH);
+    }
+    else
+    {
+      digitalWrite(RelayPin, LOW);
+    }
   }
 }
 
 void steamSwitch()
 {
-
   steamsw.read(); // read the steam switch
 
   if (steamsw.isPressed() && !steamMode) // if switch is on, set steamMode to true
@@ -348,17 +360,16 @@ void steamSwitch()
   {
     if (steamTimeMillis / 1000 <= steamReset)
     {
-      operMode = true;
-      runTimeStart = now;
       steamTimeMillis = 0;
+      enablePID(true);
     }
   }
 
   // This must be the last if statement. It's a safety check to ensure
-  // that we set operMode to false if the steam switch has been on too long
+  // that we set enablePID to false if the steam switch has been on too long
   if (steamTimeMillis / 1000 / 60 >= steamMaxMins)
   {
-    operMode = false;
+    enablePID(false);
   }
   else if (steamMode && operMode && Setpoint != SteamSetpoint)
   {
@@ -483,6 +494,9 @@ void handleRoot()
   }
   String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
   s += "<title>Espresso PID Controller</title></head><body>Espresso PID Controller";
+  s += "<br>\n";
+  s += "Go to <a href='stats'>stats page</a> for controller stats.";
+  s += "<br>\n";
   s += "Go to <a href='config'>configure page</a> to change settings.";
   s += "</body></html>\n";
 
@@ -492,38 +506,39 @@ void handleRoot()
 // ESP8266WebServer handler
 void handleStats()
 {
-  String message = "<head> <meta http-equiv=\"refresh\" content=\"2\"> </head>\n";
-  message += "<h1>\n";
-  message += "Time: " + String(runTimeSecs) + "<br>\n";
-  message += "Setpoint: " + String(Setpoint) + "<br>\n";
-  message += "PID Input:  " + String(Input) + "<br>\n";
-  message += "PID Output: " + String(Output) + "<br>\n";
-  message += "Avg: " + String(average) + "<br>\n";
-  message += "Vout: " + String(Vout) + "<br>\n";
-  message += "Loops: " + String(currLoops) + "<br>\n";
-  message += "OperMode: " + String(operMode) + "<br>\n";
-  message += "SteamMode: " + String(steamMode);
-  server.send(200, "text/html", message);
+  String br = "<br>\n";
+  String m = "<head> <meta http-equiv=\"refresh\" content=\"2\"> </head>\n";
+  m += "<h1>\n";
+  m += "Time: " + String(runTimeSecs) + br;
+  m += "Setpoint: " + String(Setpoint) + br;
+  m += "PID Input:  " + String(Input) + br;
+  m += "PID Output: " + String(Output) + br;
+  m += "Avg: " + String(average) + br;
+  m += "Vout: " + String(Vout) + br;
+  m += "Loops: " + String(currLoops) + br;
+  m += "OperMode: " + String(operMode) + br;
+  m += "SteamMode: " + String(steamMode);
+  server.send(200, "text/html", m);
 }
 
 // ESP8266WebServer handler
 void handleJSON()
 {
   // Quick and dirty JSON output without the use of ArduinoJson
-  String comma = ", ";
-  String message = "{ ";
-  message += "\"Uptime\":" + String(now / 1000) + comma;
-  message += "\"Runtime\":" + String(runTimeSecs) + comma;
-  message += "\"Setpoint\":" + String(Setpoint) + comma;
-  message += "\"Input\":" + String(Input) + comma;
-  message += "\"Output\":" + String(Output) + comma;
-  message += "\"ADC\":" + String(average) + comma;
-  message += "\"Vout\":" + String(Vout) + comma;
-  message += "\"Mode\":" + String(operMode) + comma;
-  message += "\"Loops\":" + String(currLoops) + comma;
-  message += "\"steamMode\":" + String(steamMode);
-  message += " }";
-  server.send(200, "application/json", message);
+  String c = ", ";
+  String m = "{ ";
+  m += "\"Uptime\":" + String(now / 1000) + c;
+  m += "\"Runtime\":" + String(runTimeSecs) + c;
+  m += "\"Setpoint\":" + String(Setpoint) + c;
+  m += "\"Input\":" + String(Input) + c;
+  m += "\"Output\":" + String(Output) + c;
+  m += "\"ADC\":" + String(average) + c;
+  m += "\"Vout\":" + String(Vout) + c;
+  m += "\"Mode\":" + String(operMode) + c;
+  m += "\"Loops\":" + String(currLoops) + c;
+  m += "\"steamMode\":" + String(steamMode);
+  m += " }";
+  server.send(200, "application/json", m);
 }
 
 // ESP8266WebServer handler
@@ -536,14 +551,13 @@ void handleSetvals()
 
   if (opmode_val == "off")
   {
-    operMode = false;
+    enablePID(false);
     message += "opermode: " + opmode_val;
     message += "\n";
   }
   else if (opmode_val == "on")
   {
-    operMode = true;
-    runTimeStart = now;
+    enablePID(true);
     message += "opermode: " + opmode_val;
     message += "\n";
   }
@@ -649,9 +663,8 @@ void setup()
 
   // PID settings
   windowStartTime = now;
-
   myPID.SetOutputLimits(0, 100);
-  myPID.SetSampleTime(100);
+  myPID.SetSampleTime(50);
 
   // initialize all the readings to 0:
   for (int thisReading = 0; thisReading < numReadings; thisReading++)
